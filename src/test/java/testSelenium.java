f1bb1d2da82b0606fa5c01aa089dc8fc34e9ca6e
ostolski.marcin <ostolski.marcin@gmail.com>
2023-01-16 21:33:06 +0100

    FindElements

--- src/test/java/testSelenium.java
+++ src/test/java/testSelenium.java
@@ -1,14 +1,55 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class testSelenium {
 
 
     @Test
-    public void OpenGooglePage(){
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://google.com");
+    public void FindElements(){
+        WebDriver driver = new SafariDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+
+
+        //clickOnMe
+
+        By buttonId = By.id("clickOnMe");
+        WebElement clickOnMeButton = driver.findElement(buttonId);
+        //clickOnMeButton.click();
+
+        //fname
+
+        By firstName = By.name("fname");
+        WebElement firstNameInput = driver.findElement(firstName);
+        //firstNameInput.sendKeys("Marcin");
+
+        //topSecret
+
+        WebElement topSecret = driver.findElement(By.className("topSecret"));
+
+        // input
+
+        WebElement input = driver.findElement(By.tagName("input"));
+        input.sendKeys("first");
+
+
+        List<WebElement> inputs  = driver.findElements(By.tagName("input"));
+        System.out.println(inputs.size());
+
+        // link text
+
+        WebElement linkText = driver.findElement(By.linkText("Visit W3Schools.com!"));
+        WebElement linkText2 = driver.findElement(By.partialLinkText("Visit W3Schools"));
+        //linkText.click();
+
+        // find by css
+
+
+
     }
 }
